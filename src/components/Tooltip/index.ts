95d324565cfb547309c8ef9e2877f6ae9c24cabd
task11 <task11@naver.com>
2024-05-26 10:48:15 +0900

    style: fix typos in code

--- src/components/Tooltip/index.ts
+++ src/components/Tooltip/index.ts
@@ -1,3 +1,3 @@
 export { default as Tooltip } from "./Tooltip";
-export { default as TooltipContainer } from "./TooltipFrame";
+export { default as TooltipFrame } from "./TooltipFrame";
 export { default as TooltipContent } from "./TooltipContent";
